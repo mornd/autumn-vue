af4c0e94d9c419911fade8a113b79623d23ee2a6
yanglinchen <1152229579@qq.com>
2022-10-30 22:41:38 +0800

    主页走马灯

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,68 +1,50 @@
 <template>
   <div class="home">
-    <!--<router-link to="/system/permission">菜单测试</router-link>-->
-    <br>
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="item in 6" :key="item" style="border-radius: 10px">
 
-    <div>
-      <p>当前位置：{{city}}</p>
-      <p>天气：{{weatherResult.text}}</p>
-      <p>温度：{{weatherResult.temp}}°</p>
-      <p>风向：{{weatherResult.windDir}}</p>
-      <p>湿度：{{weatherResult.humidity}}</p>
-      <p>更新时间：{{weatherResult.updateTime}}</p>
-    </div>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
   import { weather_key } from '@/constants/systemConsts'
+
   // @ is an alias to /src
 
   export default {
     name: 'Home',
     data() {
       return {
-        city:"广州",
-
-        weatherResult: {
-          text: '',
-          temp: '',
-          windDir: '',
-          updateTime: '',
-          humidity: ''
-        }
       }
     },
     mounted() {
-      this.getWeatherData()
+
     },
     methods: {
-      getWeatherData() {
-        this.searchWeather()
-      },
-      searchWeather:async function(){
-        //在请求的时候需要用async+await让它同步，否则数据不好取出
-        let key = 'f92adeb022734c6e89dd464078b63ff4'//引号中放入前面保存的key
-
-        //获取城市的ID
-        let httpUrl = `https://geoapi.qweather.com/v2/city/lookup?location=${this.city}&key=${key}`
-        let res = await fetch(httpUrl)
-        let result = await res.json()
-        let id = result.location[0].id
-        //根据城市id获取具体的天气
-        let httpUrl1 = `https://devapi.qweather.com/v7/weather/now?location=${id}&key=${key}`
-        let res2 = await fetch(httpUrl1)
-        let result2 = await res2.json()
-        console.log(result2);
-        this.weatherResult.text = result2.now.text
-        this.weatherResult.temp = result2.now.temp
-        this.weatherResult.windDir = result2.now.windDir
-        this.weatherResult.humidity = result2.now.humidity
-        this.weatherResult.updateTime = result2.updateTime
-      }
     }
   }
 </script>
 
 <style scoped>
+  .home {
+    padding: 5px;
+  }
+
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
